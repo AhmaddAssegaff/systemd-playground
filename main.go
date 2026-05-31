1f012a5d31edf63eb93baeca2bc51ab0a5a72e6c
Ahmad <ahmadasgf89@gmail.com>
2026-05-31 13:23:04 +0700

    chore: add env on the project

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -18,14 +19,18 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("request from %s", r.RemoteAddr)
-
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Hello World"))
 	})
 
-	log.Println("server started on :8080")
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("server started on :" + port)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
 }
